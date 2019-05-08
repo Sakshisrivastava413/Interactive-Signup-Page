2790168fdb99e6480789897c0ae1e6e68213d51d
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-09 01:52:34 +0530

    --update: removed useless pages declarations and added signup component in declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SignupPage } from '../pages/signup/signup';
 
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+   SignupPage
   ],
   imports: [
     BrowserModule,
@@ -24,9 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    SignupPage
   ],
   providers: [
     StatusBar,
