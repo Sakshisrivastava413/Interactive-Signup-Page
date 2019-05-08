de7b4309ebc53014db2fc19f4d12287a5db007a4
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-09 01:55:25 +0530

    --update: shifted the input box template from signup page toits resp. component

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
+import { InputBoxComponent } from '../../components/input-box/input-box';
 
 @NgModule({
   declarations: [
     SignupPage,
+    InputBoxComponent
   ],
   imports: [
     IonicPageModule.forChild(SignupPage),
