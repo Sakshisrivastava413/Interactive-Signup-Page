b32fa5f7333e1b0f28491a07589d6bd0a328e4b5
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-10 01:43:54 +0530

    --fix: some changes due to conflicts

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { InputBoxComponent } from './input-box/input-box';
-@NgModule({
-	declarations: [InputBoxComponent],
-	imports: [],
-	exports: [InputBoxComponent]
-})
-export class ComponentsModule {}
