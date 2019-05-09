3e5bead7bb1c4d99eb9e04a2f8acc38815f91872
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-10 04:27:59 +0530

    --add: subscription card component with mock card

---
+++ src/app/components/subscription-card/subscription-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-card',
+  templateUrl: './subscription-card.component.html',
+  styleUrls: ['./subscription-card.component.scss'],
+})
+export class SubscriptionCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
