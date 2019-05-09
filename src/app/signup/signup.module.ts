d8d73e0de621053435519a0a4f68f71889947aa3
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-09 23:42:27 +0530

    --MAJOR: migrated the codebase from ionic 3 to ionic 4

---
+++ src/app/signup/signup.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SignupPage } from './signup.page';
+import { InputBoxComponent } from '../components/input-box/input-box.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SignupPage, InputBoxComponent]
+})
+export class SignupPageModule {}
