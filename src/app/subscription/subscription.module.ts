340a7d68611c432f7386f9f041a675d59741a8f0
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-10 16:25:56 +0530

    added card component in its page module to use it

--- src/app/subscription/subscription.module.ts
+++ src/app/subscription/subscription.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { SubscriptionPage } from './subscription.page';
+import { SubscriptionCardComponent } from '../components/subscription-card/subscription-card.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [SubscriptionPage]
+  declarations: [SubscriptionPage, SubscriptionCardComponent]
 })
 export class SubscriptionPageModule {}
