6726b77fd175afff11028646879442e88e820724
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-05-10 18:43:34 +0530

    --fix: removed mock data and using the values passed from parent

--- src/app/components/subscription-card/subscription-card.component.ts
+++ src/app/components/subscription-card/subscription-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-subscription-card',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionCardComponent implements OnInit {
 
+  @Input() duration: String;
+  @Input() price: Number;
+  
   constructor() { }
 
   ngOnInit() {}
